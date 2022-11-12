f4d06fda0a8d5f87705d615d8d65c0970da53938
Ademola <ademolaogunmokun@yahoo.com>
2022-11-12 08:50:46 +0100

    set memo to the coinexcerpt

--- src/screens/CoinDetailsScreen.jsx
+++ src/screens/CoinDetailsScreen.jsx
@@ -1,50 +1,74 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
-import { LineChart, Grid, YAxis }  from  'react-native-svg-charts';
+import { LineChart, Grid, YAxis, XAxis }  from  'react-native-svg-charts';
 export default function CoinDetailsScreen() {
   const route = useRoute();
   const {  marketData, coin } = route.params;
-  console.log(marketData);
-  const priceAndDate = marketData?.prices.map((item) => {
+  const priceAndDate = marketData.prices.map((item) => {
     return {
       price: item[1],
       date: new Date(item[0]).toLocaleDateString(),
     };
   });
 
-  console.log(priceAndDate);
   return (
     <View>
 
       <Text style = {styles.header}>{
         coin.name
       } price </Text>
-      <YAxis
-        data = {priceAndDate.map((item) => item.price)}
-        contentInset = {{ top: 10, bottom: 10 }}
-        svg = {{
+      <Text>
+        <YAxis
+          data={priceAndDate.map((item) => item.price)}
+          contentInset={{ top: 10, bottom: 10 }}
+          svg={{
+            fill: 'grey',
+            fontSize: 10,
+          }}
+          numberOfTicks={10}
+          formatLabel={(value) => `$${value}`}
+          transform={{ translateX: 10 }}
+          style= {{
+            height: 200,
+            width: 50,
 
-          fill: 'grey',
-          fontSize: 10,
-          styles : {
-            transform: [{ rotate: '90deg' }],
-            color: 'white',
-          },
-        }
-        }
-        numberOfTicks = {10}
-        formatLabel = {(value) => `$${value}`}
-      />
-      <LineChart
-        style={{ height: 200, flex: 1, marginLeft: 16 }}
-        data={priceAndDate.map((item) => item.price)}
-        svg={{ stroke: 'rgb(134, 65, 244)' }}
-        contentInset={{ top: 20, bottom: 20 }}
+          }}
+        />
+        <LineChart
+          style={{ height: 200, flex: 1, marginLeft: 16,
+            color: 'red',
+            width : 300,
+            alignSelf: 'center',
 
-      >
-        <Grid />
-      </LineChart>
+          }}
+
+
+          data={priceAndDate.map((item) => item.price)}
+          svg={{ stroke: 'rgb(113, 65, 244)' }}
+          contentInset={{ top: 20, bottom: 20 }}
+
+
+        >
+          <Grid />
+
+
+
+        </LineChart>
+        <XAxis
+          style={{ marginHorizontal: -10,
+            height: 300,
+            width: 300,
+          }}
+          data={priceAndDate.map((item) => item.date)}
+          formatLabel={(value, index) => index}
+          contentInset={{ left: 10, right: 10 }}
+          svg={{ fontSize: 10, fill: 'grey', }}
+        />
+      </Text>
+
+
+
     </View>
   );
 }
