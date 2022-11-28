1ff07f018f0b075dc66b6a7221006064b11e3e6b
Ademola <ademolaogunmokun@yahoo.com>
2022-11-28 18:26:38 +0100

    change version number

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ export default {
       backgroundColor: '#FFFFFF'
     },
     package: 'com.preview.assetapp',
-    versionCode: 3
+    versionCode: 4
   },
   web: {
     favicon: './assets/favicon.png'
